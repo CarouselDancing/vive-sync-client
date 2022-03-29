5b37a0b8b159e6c2d5fcce36daaa23a9ee383864
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-03-29 02:28:25 +0200

    Moved: tracker config to vrclient and deactivated network sync when debug vis is active

--- Assets/VRAvatarClient/Scripts/GameManager.cs
+++ Assets/VRAvatarClient/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class GameManager : MonoBehaviour
 {
     public GlobalGameState state;
     public GameObject networkClient;
     public GameObject offlineTest;
-    
+    VRRigConfig trackerConfig;
+
     void Start()
     {
+        trackerConfig = Camera.main.GetComponent<VRRigConfig>();
         state = GlobalGameState.GetInstance();
+        ConfigureRig();
 
         if (state.config.activateDebugVis)
         {
@@ -22,6 +26,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void ConfigureRig()
+    {
+        if (trackerConfig == null)
+        {
+            return;
+        }
+        ConfigureTracker(trackerConfig.hipTrackerTarget, state.config.hipTracker);
+        ConfigureTracker(trackerConfig.leftFootTarget, state.config.leftFootTracker);
+        ConfigureTracker(trackerConfig.rightFootTarget, state.config.rightFootTracker);
+    }
     
+
+    public void ConfigureTracker(TrackerTarget tracker, TrackerConfig config)
+    {
+        var ht = trackerConfig.hipTrackerTarget.GetComponent<SteamVR_TrackedObject>();
+        var _offset = config.posOffset;
+        tracker.offset = new Vector3(_offset[0], _offset[1], _offset[2]);
+        var _rotOffset = config.rotOffset;
+        tracker.rotationOffset = new Vector3(_rotOffset[0], _rotOffset[1], _rotOffset[2]);
+        var t = tracker.GetComponent<SteamVR_TrackedObject>();
+        t.SetDeviceIndex(config.deviceID);
+        Debug.Log("set device id" + name + ": " + config.deviceID.ToString());
+    }
+
+
+
 
 }
