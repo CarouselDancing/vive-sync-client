91754632ca810a0547819f7d4259813333a884a7
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-04-07 16:19:35 +0200

    Updated ReadyPlayerMe SDK

--- Assets/VRAvatarClient/Scripts/RPMAvatarTest.cs
+++ Assets/VRAvatarClient/Scripts/RPMAvatarTest.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Wolf3D.ReadyPlayerMe.AvatarSDK;
+using ReadyPlayerMe;
 
 
 public class RPMAvatarTest : MonoBehaviour
